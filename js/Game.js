151de820081f5369c98ed3c5ee49d20fa8bcef09
Ahmed Roble <airoble2019@gmail.com>
2021-06-23 18:41:38 -0400

    Began Game and Phrase classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0
+    this.phrases = this.createPhrases()
+    this.activePhrase = null
+  }
+
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases() {
+    const phrases = [
+      new Phrase("Don't be stupid it might make you famous"),
+      new Phrase("I am on a seafood diet"),
+      new Phrase("May the force be with you"),
+      new Phrase("Life will find a way"),
+      new Phrase("There is no place like home"),
+      new Phrase("Elementary my dear Watson"),
+    ]
+    return phrases
+  }
+
+  startGame() {
+    document.getElementById("overlay").style.display = "none"
+    Phrase
+    this.activePhrase = this.getRandomPhrase()
+  }
+
+  /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+  getRandomPhrase() {
+    return this.phrases[Math.floor(Math.random() * this.phrases.length)]
+  }
+  handleInteraction() {}
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,39 @@
+"use strict"
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase()
+  }
+  addPhraseToDisplay() {
+    let html
+    phraseArray = this.phrase.split()
+    phraseArray.forEach((ele) => {
+      if (ele === " ") {
+        html += `<li class="space"> </li>`
+      } else {
+        ;`<li class="hide letter ${ele}">${ele}</li>`
+      }
+    })
+  }
+  checkLetter() {
+    document.getElementById("qwerty").addEventListener("click", () => {
+      const isMatch =
+        e.target.value === document.querySelector(`.${e.target.value}`).value
+          ? true
+          : false
+    })
+    return isMatch
+  }
+  showMatchedLetter() {
+    if (this.checkLetter()) {
+      const matchedLetters = document.querySelectorAll(`.${e.target.value}`)
+      matchedLetters.forEach((letter) => {
+        letter.classList.remove("hide")
+        letter.classList.add("show")
+      })
+    }
+  }
+}
